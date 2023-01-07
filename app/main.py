b43c9638855706d40eee6bfec18e17bf704520a8
ktny <katsunori.t10@gmail.com>
2023-01-07 18:24:20 +0900

    filesystem isolation

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
+import os
+import shutil
 import subprocess
 import sys
+
+tmp_dir = "/home/tmp"
+executable_file = "/usr/local/bin/docker-explorer"
 
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
+
+    # create tmp dir & copy executable into tmp dir
+    os.makedirs(f"{tmp_dir}{os.path.dirname(executable_file)}", exist_ok=True)
+    shutil.copy(executable_file, f"{tmp_dir}{executable_file}")
+
+    # change root dir
+    os.chroot(tmp_dir)
 
     completed_process = subprocess.run([command, *args], capture_output=True)
     sys.stdout.buffer.write(completed_process.stdout)
